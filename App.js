495ebd969131fb90ae76cfa24e690d5874f0322c
Michael Henderson <mikesmac@Michaels-MacBook-Pro.local>
2024-04-02 13:46:12 -0400

    Initial commit

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import cors from 'cors';
 
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
+import CourseRoutes from './Kanbas/courses/routes.js';
+import ModuleRoutes from './Kanbas/modules/routes.js';
 
 const app = express()
 app.use(cors());
 app.use(express.json());
 
+CourseRoutes(app);
+ModuleRoutes(app);
 Hello(app);
 Lab5(app);
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
