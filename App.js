3f6ceeae17745cfff14d1c5c4ae718cb17e4cca7
Michael Henderson <mikesmac@Michaels-MacBook-Pro.local>
2024-04-01 23:46:00 -0400

    completed lab portion of assignment 5

---
+++ App.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+import Hello from './Hello.js';
+import Lab5 from './Lab5.js';
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+
+Hello(app);
+Lab5(app);
+
+app.listen(4000)
