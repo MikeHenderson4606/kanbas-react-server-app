aa2b7debc5e3fba89d6dc7d8ed53c29eb0ce420b
Michael Henderson <mikesmac@Michaels-MBP.hsd1.ma.comcast.net>
2024-04-16 18:43:19 -0400

    Working on a6

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
+import "dotenv/config";
+import session from 'express-session';
 
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
+import UserRoutes from './Users/routes.js';
 
+const connectionString = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 const app = express()
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
+mongoose.connect(connectionString);
 
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 
